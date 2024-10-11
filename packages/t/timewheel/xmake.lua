c07aaa5e8b4bdc01b5c7b1bcf4db4d11a78ee1ae
odorajbotoj <odorajbotoj@163.com>
2024-10-11 22:59:06 +0800

    update timewheel repo

--- packages/t/timewheel/xmake.lua
+++ packages/t/timewheel/xmake.lua
@@ -3,3 +3,7 @@ package("timewheel")
     set_description("header-only simple timewheel")
 
     add_urls("https://github.com/CoralFans-Dev/timewheel.git")
+
+    on_install(function (package)
+        import("package.tools.xmake").install(package, configs)
+    end)
